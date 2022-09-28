56dc83f78141dcc61c944ec4eb276750bdd936ed
u31110 <const776@mail.ru>
2022-09-28 11:43:13 +1000

    G

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -28,45 +28,44 @@ namespace WPF_Shovkun_PR3
         private void Pusk_Click(object sender, RoutedEventArgs e)
         {
             double x = Convert.ToDouble(X.Text);
-            double y = Convert.ToDouble(Y.Text);
-            double z = Convert.ToDouble(Z.Text);
-            Itog.Text = "Результат" + Environment.NewLine;
+            Itog.Text = "Результат:" + Environment.NewLine;
             Itog.Text += "При X = " + X.Text + Environment.NewLine;
-            Itog.Text += "При Y = " + Y.Text + Environment.NewLine;
-            Itog.Text += "При Z = " + Z.Text + Environment.NewLine;
 
             int n = 0;
-            double u;
+            double z;
 
-            if (RB2.IsChecked == true) n = 1;
-            else if (RB3.IsChecked == true) n = 2;
+             if (RB2.IsChecked == true) n = 1;
+             else if (RB3.IsChecked == true) n = 2;
 
-            switch (n) 
-            {
+            switch (n)
+            {              
                 case 0:
-                    if ((z - x) == 0) u = y * Math.Sin(x) * Math.Sin(x) + z;
-                    else if ((z - x) < 0) u = y * Math.Exp(Math.Sin(x)) - z;
-                    else u = y * Math.Sin(Math.Sin(x)) + z;
-                    Itog.Text += "U = " + Convert.ToString(u) + Environment.NewLine;
-                break;
-                case 1:
-                    if ((z - x) == 0) u = y * Math.Cos(x) * Math.Cos(x) + z;
-                    else if ((z - x) < 0) u = y * Math.Exp(Math.Cos(x)) - z;
-                    else u = y * Math.Sin(Math.Cos(x)) + z;
-                    Itog.Text += "U = " + Convert.ToString(u) + Environment.NewLine;
-                break;
-                case 2:
-                    if ((z - x) == 0) u = y * Math.Exp(x) * Math.Exp(x) + z;
-                    else if ((z - x) < 0) u = y * Math.Exp(Math.Exp(x)) - z;
-                    else u = y * Math.Sin(Math.Exp(x)) + z;
-                    Itog.Text += "U = " + Convert.ToString(u) + Environment.NewLine;
+                    if (x >= 0 && x <= 3)
+                    {
+                        z = x * x;
+                    }
+
+                    else 
+                        z = x * x;                                      
+                    Itog.Text += "Z = " + Convert.ToString(z) + Environment.NewLine;
                     break;
-                default:
-                    Itog.Text += "Решение не найдено" + Environment.NewLine;
-                break;
+                case 1:
+                    if (x > 3 && x < 0)
+                    {
+                        z = 4 * x;
+                    }
+                    else
+                        z = 4 * x;
+                    Itog.Text += "Z = " + Convert.ToString(z) + Environment.NewLine;
+                    break;
             }
+
         }
-        
-        
+
+        private void Clean_Click(object sender, RoutedEventArgs e)
+        {
+            X.Clear();
+            Itog.Clear();
+        }
     }
 }
